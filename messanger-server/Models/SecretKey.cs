2a9fab3d9824be2ddf135faae20f8c97ff2ad67a
TopAcademy2022 <popov_r_top@outlook.com>
2024-01-14 17:57:51 +0300

    Added connection to database. Closed #4

---
+++ messanger-server/Models/SecretKey.cs
@@ -0,0 +1,9 @@
+﻿namespace messanger_server.Models
+{
+    public class SecretKey
+    {
+        public int Id { get; set; }
+
+        public string Key { get; set; }
+    }
+}
---
+++ messanger-server/Services/DatabaseConnection.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using messanger_server.Models;
+
+namespace messanger_server.Services
+{
+    public class DatabaseConnection : DbContext
+    {
+        private const string _CONFIG_FILE_NAME = "docker-compose.yaml";
+
+        private string _textConfigurationFile;
+
+        private string _databaseConnectionString;
+
+        private DatabaseTypes? _selectedDatabaseType;
+
+        private enum DatabaseTypes
+        {
+            MSSQL = 0
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+
+        public DbSet<SecretKey> SecretKeys { get; set; } = null!;
+
+        private string GetTextFromConfigFile()
+        {
+            // Replace file path
+            const string CONFIG_FILE_DIRECTORY = "./";
+            string result = string.Empty;
+            
+            try
+            {
+                FileStream fileStream = File.OpenRead(CONFIG_FILE_DIRECTORY + _CONFIG_FILE_NAME);
+                byte[] bufer = new byte[fileStream.Length];
+                fileStream.Read(bufer);
+
+                result = Encoding.Default.GetString(bufer);
+
+                fileStream.Close();
+            }
+            catch(Exception ex)
+            {
+                // Log ex.Message
+            }
+
+            return result;
+        }
+
+        private DatabaseTypes? GetDatabaseType()
+        {
+            if (this._textConfigurationFile.Contains("sqldata"))
+            {
+                return DatabaseTypes.MSSQL;
+            }
+
+            return null;
+        }
+
+        private string GetConnectionString()
+        {
+            string result = string.Empty;
+
+            if (this._selectedDatabaseType == DatabaseTypes.MSSQL)
+            {
+                // Add parse config file
+                result = @"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;";
+            }
+
+            return result;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            switch (this._selectedDatabaseType)
+            {
+                case DatabaseTypes.MSSQL:
+                    dbContextOptionsBuilder.UseSqlServer(this._databaseConnectionString);
+                    break;
+                default:
+                    // Log error
+                    break;
+            }
+        }
+
+        public DatabaseConnection()
+        {
+            this._textConfigurationFile = this.GetTextFromConfigFile();
+            this._selectedDatabaseType = this.GetDatabaseType();
+            this._databaseConnectionString = this.GetConnectionString();
+            // Add init database tables
+        }
+    }
+}

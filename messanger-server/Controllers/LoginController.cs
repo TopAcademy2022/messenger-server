dd3c82d5e037caeda0066a98c9a9368f64b071c7
M_VALRUS <99125211+M-VALRUS@users.noreply.github.com>
2024-02-11 15:12:00 +0300

    Add simple login controller

---
+++ messanger-server/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using messanger_server.Services;
+using messanger_server.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace messanger_server.Controllers
+{
+    [ApiController]
+    [Route("login")]
+    public class LoginController : ControllerBase
+    {
+        private IUserServise _userServise;
+
+        public LoginController()
+        {
+            this._userServise = new UserService();
+        }
+
+        [HttpPost]
+        public string Post(string login, string password)
+        {
+            const string SUCCESS = "successfully logged in!";
+            const string FAILED = "failed to log in...";
+
+            if (ModelState.IsValid)
+            {
+                if (this._userServise.CheckCorrectLogin(login) &&
+                this._userServise.CheckCorrectPassword(password) &&
+                this._userServise.LoginAttempt(login, password))
+                {
+                    return SUCCESS;
+                }
+            }
+
+            return FAILED;
+        }
+    }
+}

ff254943a68f4b93213f2c41832821018695f244
cany8042 <soffitto69@yandex.ru>
2024-07-05 20:49:18 +0300

    Added implementation CheckCorrectLogin method. Closed #9

--- messenger-server/Services/UserService.cs
+++ messenger-server/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using messenger_server.Models.Entities;
+﻿using System.Text.RegularExpressions;
+using messenger_server.Models.Entities;
 using messenger_server.Services.Interfaces;
 
 // Add documentation
@@ -55,6 +56,26 @@ namespace messenger_server.Services
         // Replace
         public bool CheckCorrectLogin(string login)
         {
+            // Проверка длины
+            if (string.IsNullOrEmpty(login) || login.Length > 20) /*Если логин пустой и его длина больше 20 то выводит false*/
+            {
+                return false;
+            }
+
+            //  Проверка на SQL-инъекции с помощью спец символов
+            string sqlInjectionPattern = @"[\?&$'|#№/\\*;]+";
+            if (Regex.IsMatch(login, sqlInjectionPattern))
+            {
+                return false;
+            }
+
+            // Проверка на XSS-инъекции с помощью спец символов
+            string xssPattern = @"[<|>]+";
+            if (Regex.IsMatch(login, xssPattern))
+            {
+                return false;
+            }
+
             return true;
         }
 
